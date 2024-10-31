efc4964081d5dd4a14da149ddb335f8fdca0cad4
Baraiboapex <31634799+Baraiboapex@users.noreply.github.com>
2024-10-31 18:24:02 -0400

    init

---
+++ MattBaileyDesignsAPI/Controllers/OutboundDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace MattBaileyDesignsAPI.Controllers
+{
+    sealed public class OutboundDTO
+    {
+        private readonly Dictionary<string, object> _dtoValue;
+
+        public OutboundDTO(Dictionary<string, object> dtoValue)
+        {
+            _dtoValue = dtoValue;
+        }
+
+        public void AddData(KeyValuePair<string, object> val) { 
+            _dtoValue.Add(val.Key, val.Value);
+        }
+
+        public Dictionary<string, object> currentDtoValue { 
+            get 
+            {
+                return _dtoValue;    
+            } 
+        }
+    }
+}
